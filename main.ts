aa3e85b61d9ba052276e2b92897785d170743187
Barry.C <wjyzxhm888@163.com>
2025-03-31 01:20:21 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -10,15 +10,21 @@ enum Pins {
     //% block="P1"
     P1 = AnalogPin.P1,
     //% block="P2"
-    P2 = AnalogPin.P2
+    P2 = AnalogPin.P2,
+    //% block="P3"
+    P3 = AnalogPin.P3
 }
 
 /**
  * mqlib blocks
  */
 //% weight=100 color=#5c2d91 icon=""
-//% groups=['servo','others']
+//% groups=['servo','led','others']
 namespace mqlib {
+
+    let _ledRedPin = 0;
+    let _ledGreenPin = 0;
+    let _ledBluePin = 0; //blue and yellow
 
     /**
      * 输入270度转为180度
@@ -53,5 +59,37 @@ namespace mqlib {
         pins.servoWritePin(p, Math.map(v, 0, 270, 0, 100));
     }
 
+    /**
+     * 设置led灯引脚和颜色
+     * @param v describe
+     */
+    //% block='设置led灯引脚 $p 颜色 $color'
+    //% group='led'
+    //% color.shadow="colorNumberPicker"
+    export function initLedPins(p: Pins, color: number): void {
+        if (color == 16711680) {
+            _ledRedPin = p;
+        } else if (color == 65280) {
+            _ledGreenPin = p;
+        } else if (color == 32767 || color == 16776960) {
+            _ledBluePin = p;
+        }
+    }
+    /**
+     * 设置led灯引脚和颜色
+     * @param v describe
+     */
+    //% block='设置led灯颜色 $color'
+    //% group='led'
+    //% color.shadow="colorNumberPicker"
+    export function setLedColor(color: number): void {
+        if (color == 16711680) {
+            pins.digitalWritePin(_ledRedPin, 1)
+        } else if (color == 65280) {
+            pins.digitalWritePin(_ledRedPin, 1)
+        } else if (color == 32767 || color == 16776960) {
+            pins.digitalWritePin(_ledRedPin, 1)
+        }
+    }
 
 }
