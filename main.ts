e6d9a59f3935b3426ebdbcd7910f802a0a1ae334
Barry.C <wjyzxhm888@163.com>
2025-04-23 16:37:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -124,4 +124,34 @@ namespace mqlib {
         }
     }
 
+    /**
+     * Create a 16x16 pixel matrix for use as a custom character.
+     */
+    //% subcategory="oled"
+    //% block="character16x16"
+    //% imageLiteral=1
+    //% imageLiteralColumns=16
+    //% imageLiteralRows=16
+    //% imageLiteralScale=0.001
+    //% shim=images::createImage
+    export function oledCharacterPixels1616(i: string): Image {
+        return <Image><any>i;
+    }
+
+    /**
+     * Create a 128x64 pixel matrix for use as a custom character.
+     */
+    //% subcategory="oled"
+    //% block
+    export function oledDrawImgWithPixels(im: Image): void {
+        for (let y = 0; y < 64; y++) {
+            for (let x = 0; x < 128; x++) {
+                if (im.pixel(x, y)) {
+                    serial.writeLine(x.toString()+'-'+y.toString())
+                    //todo call < OLED12864_I2C.pixel(x, y, color) >
+                }
+            }
+        }
+    }
+
 }
