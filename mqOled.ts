e06fbb2fb415747d30f2505cbe8b72b015ce5d03
Barry.C <wjyzxhm888@163.com>
2025-05-23 00:40:31 +0800

    Create mqOled.ts

---
+++ mqOled.ts
@@ -0,0 +1,14 @@
+/**
+* 使用此文件来定义自定义函数和图形块。
+* 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+*/
+/**
+ * mqMath blocks
+ */
+namespace mqlib {
+    //% subcategory="a"
+    //% block="test"
+    export function test(v: number) {
+        
+    }
+}
