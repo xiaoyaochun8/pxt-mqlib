628fce53040cd79ac9c18897276edeb8cfa60397
Barry.C <wjyzxhm888@163.com>
2025-05-22 08:33:09 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -26,11 +26,24 @@ enum DgPins {
     P3 = DigitalPin.P3
 }
 
+enum OledFace {
+    //% block="开心"
+    Happy,
+    //% block="伤心"
+    Sad,
+    //% block="生气"
+    Angry,
+    //% block="惊讶"
+    Amazed,
+    //% block="睡觉"
+    Sleep,
+}
+
 /**
  * mqlib blocks
  */
 //% weight=100 color=#5c2d91 icon=""
-//% groups=['舵机270','Servo180','Led','others']
+//% groups=['舵机270','Servo180','Led','others','oled-形状','oled-表情','oled-画图']
 namespace mqlib {
 
     let _ledRedPin = 0;
@@ -127,10 +140,64 @@ namespace mqlib {
         }
     }
 
+    //% subcategory="oled"
+    //% group='oled-形状'
+    //% block="oled画正方形 $iSize"
+    //% iSize.min=1 iSize.max=3 iSize.defl=1
+    export function oledDrawSquare(iSize: Number): void {
+        OLED12864_I2C.init(60);
+        if (iSize == 3) {
+            OLED12864_I2C.rect(0, 0, 30, 30, 1);
+        } else if (iSize == 2) {
+            OLED12864_I2C.rect(0, 0, 20, 20, 1);
+        } else {
+            OLED12864_I2C.rect(0, 0, 10, 10, 1);
+        }
+    }
+    //% subcategory="oled"
+    //% group='oled-形状'
+    //% block="oled画长方形 $iSize"
+    //% iSize.min=1 iSize.max=3 iSize.defl=1
+    export function oledDrawRectangle(iSize: Number): void {
+        OLED12864_I2C.init(60);
+        if (iSize == 3) {
+            OLED12864_I2C.rect(0, 0, 60, 30, 1);
+        } else if (iSize == 2) {
+            OLED12864_I2C.rect(0, 0, 40, 20, 1);
+        } else {
+            OLED12864_I2C.rect(0, 0, 20, 10, 1);
+        }
+    }
+    //% subcategory="oled"
+    //% group='oled-表情'
+    //% block="oled画表情 $oledFace"
+    //% gesture.fieldEditor="gridpicker"
+    //% gesture.fieldOptions.width=220
+    //% gesture.fieldOptions.columns=3
+    export function oledDrawFace(oledFace: OledFace): void {
+        OLED12864_I2C.init(60);
+        let im = `
+# · ·
+# · ·
+# · ·
+        `;
+        if (oledFace == OledFace.Happy) {
+            //im = '';
+        }
+        for (let y = 0; y < 32; y++) {
+            for (let x = 0; x < 64; x++) {
+                //if (im.pixel(x, y)) {
+                OLED12864_I2C.pixel(x, y, 1)
+                //}
+            }
+        }
+    }
+    
     /**
      * Create a 128x64 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
+    //% group='oled-画图'
     //% block="oled点阵128x64"
     //% imageLiteral=1
     //% imageLiteralColumns=64
@@ -145,6 +212,7 @@ namespace mqlib {
      * Create a 128x64 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
+    //% group='oled-画图'
     //% block="oled画图128x64 $im"
     export function oledDrawImgWithPixels12864(im: Image): void {
         OLED12864_I2C.init(60)
@@ -161,6 +229,7 @@ namespace mqlib {
      * Create a 13x13 pixel matrix for use as a custom character.
      */
     //% subcategory="oled"
+    //% group='oled-画图'
     //% block="oled点阵13x13"
     //% imageLiteral=1
     //% imageLiteralColumns=13
@@ -170,4 +239,6 @@ namespace mqlib {
     export function oledCharacterPixels1313(i: string): Image {
         return <Image><any>i;
     }
+
+    
 }
