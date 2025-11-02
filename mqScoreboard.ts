a933172973bf28ce8715d5c268cd2fcd7967acc5
Barry.C <wjyzxhm888@163.com>
2025-11-02 12:23:57 +0800

    更新 mqScoreboard.ts

--- mqScoreboard.ts
+++ mqScoreboard.ts
@@ -2,15 +2,23 @@
  * mqScoreboard blocks
  */
 namespace mqlib {
-    let leftName = '';
-    let rightName = '';
-    let leftScore = 0;
-    let rightScore = 0;
+    let _leftName = '';
+    let _rightName = '';
+    let _leftScore = 0;
+    let _rightScore = 0;
+    let _scoreboardStr = '';
 
     //% subcategory="计分板"
-    //% block="初始化计分板 左边名字 右边名字"
-    //% v.defl=1
-    export function initScoreboard(v: number) {
-        
+    //% block="初始化计分板 左边名字 $leftName 右边名字 $rightName"
+    export function initScoreboard(leftName: string, rightName: string) {
+        _leftName = leftName;
+        _rightName = rightName;
     }
+
+    //% subcategory="计分板"
+    //% block="计分板内容"
+    export function getScoreboardStr(): string {
+        return _scoreboardStr;
+    }
+
 }
